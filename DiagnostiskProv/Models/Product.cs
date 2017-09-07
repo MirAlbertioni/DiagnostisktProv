e15f272b2ef01aaa445a8fb50132827db3e5f13a
Mir Albertioni <mir.al@live.se>
2017-09-07 10:34:41 +0200

    validation attribut added to name and price

--- DiagnostiskProv/Models/Product.cs
+++ DiagnostiskProv/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace DiagnostiskProv.Models
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Range(minimum: 5, maximum: 1000, ErrorMessage = "Must be between 5 and 1000")]
         public decimal Price { get; set; }
     }
 }
